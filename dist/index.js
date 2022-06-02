52ec30ea96cacdd5b7f098de7f465c1e6c6a1f33
Serhii Plastun <scoced@gmail.com>
2022-06-02 23:13:21 +0300

    create

--- dist/index.js
+++ dist/index.js
@@ -7,11 +7,15 @@ const express_1 = __importDefault(require("express"));
 const constants_1 = require("./constants/constants");
 const router_1 = require("./router/router");
 const refreshCurrencies_1 = require("./service/refreshCurrencies");
+const axios_1 = __importDefault(require("axios"));
 const cron = require('node-cron');
 const app = (0, express_1.default)();
 app.use(router_1.router);
 cron.schedule('*/5 * * * *', () => {
     (0, refreshCurrencies_1.refreshCurrencies)().then(r => console.log('running a task every 5 minute')).catch(err => console.error("error in refreshCurrencies", err));
+});
+cron.schedule('*/29 * * * *', () => {
+    axios_1.default.get('https://calm-lowlands-23052.herokuapp.com/');
 });
 app.listen(constants_1.PORT, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${constants_1.PORT}`);
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, {Express} from 'express';
 import {PORT} from "./constants/constants";
 import {router} from "./router/router"
 import {refreshCurrencies} from "./service/refreshCurrencies";
+import axios from "axios";
 
 const cron = require('node-cron');
 
@@ -12,6 +13,10 @@ cron.schedule('*/5 * * * *', () => {
     refreshCurrencies().then(r => console.log('running a task every 5 minute')).catch(err =>
         console.error("error in refreshCurrencies", err)
     );
+});
+
+cron.schedule('*/29 * * * *', () => {
+    axios.get('https://calm-lowlands-23052.herokuapp.com/');
 });
 
 app.listen(PORT, () => {
